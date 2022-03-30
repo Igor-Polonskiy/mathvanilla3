0978e9ff396b613150043f95a6d3bc2e6474c4ce
Polonskiy Igor <barlogfire@ya.ru>
2022-03-30 17:55:01 +0300

    less5 task1-6

---
+++ js-data/assets5/task5_script.js
@@ -0,0 +1,88 @@
+(() => {
+    const reloadTaskBtn = document.querySelector('.task5_reloadTask')
+    const checkingTaskBtn = document.querySelector('.task5_checkingTask')
+    const checkTask = document.querySelector('.task5_checkTask')
+    const chek_answerTxt = document.querySelector('.task5_chek_answer')
+    const imgsWrapper = document.querySelector('.task5_imgwrapper')
+
+    let pictures = [{
+            id: 1,
+            src: 'Images_3/assets5/task5_1.png',
+            data: true
+        },
+        {
+            id: 2,
+            src: 'Images_3/assets5/task5_2.png',
+            data: false
+        },
+        {
+            id: 3,
+            src: 'Images_3/assets5/task5_3.png',
+            data: false
+        },
+        {
+            id: 4,
+            src: 'Images_3/assets5/task5_4.png',
+            data: false
+        },
+        {
+            id: 5,
+            src: 'Images_3/assets5/task5_5.png',
+            data: true
+        },
+        {
+            id: 5,
+            src: 'Images_3/assets5/task5_6.png',
+            data: false
+        }
+    ]
+
+
+    let count = 0
+
+    function fillField() {
+        pictures.sort(() => Math.random() - 0.5).forEach(item => {
+            let answer = document.createElement('div')
+            answer.classList.add('task5_img')
+            answer.style.backgroundImage = `url(${item.src})`
+            answer.setAttribute('data-number', item.data)
+            imgsWrapper.append(answer)
+
+        })
+    }
+
+    fillField()
+
+    imgsWrapper.addEventListener('click', (e) => {
+        if (e.target.classList.contains('task5_img')) {
+            e.target.classList.toggle('task5_active')
+        }
+    })
+
+    reloadTaskBtn.addEventListener('click', () => {
+        imgsWrapper.innerHTML = ''
+        fillField()
+
+        chek_answerTxt.innerHTML = ''
+        checkTask.style.background = ''
+    })
+
+    checkingTaskBtn.addEventListener('click', () => {
+        let imgs = document.querySelectorAll('.task5_active')
+        let winVar = 0
+        imgs.forEach(item => {
+            if (item.getAttribute('data-number') === 'true') {
+                winVar++
+            } else winVar--
+        })
+
+        if (winVar === 2) {
+            chek_answerTxt.innerHTML = '<span>&#128077;</span> Молодец!'
+            checkTask.style.background = 'lightgreen'
+        } else {
+            chek_answerTxt.innerHTML = '<span>&#10060;</span> Попробуй еще!'
+            checkTask.style.background = 'lightpink'
+        }
+    })
+
+})()
